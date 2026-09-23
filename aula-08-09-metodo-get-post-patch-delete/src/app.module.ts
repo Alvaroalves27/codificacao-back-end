3728d08713d3a5ee691fd9bcc31a039fe790f1c9
joaovitor <joaovitorx011@gmail.com>
2026-09-22 21:46:25 -0300

    feat: configura modulo de convidados

--- aula-08-09-metodo-get-post-patch-delete/src/app.module.ts
+++ aula-08-09-metodo-get-post-patch-delete/src/app.module.ts
@@ -3,12 +3,13 @@ import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { ConvidadosController } from './convidados.controller.js';
+import { ConvidadosService } from './convidados.service.js';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
 @Module({
   imports: [],
   controllers: [AppController,ConvidadosController],
-  providers: [AppService],
+  providers: [AppService, ConvidadosService],
 })
 export class AppModule {}
